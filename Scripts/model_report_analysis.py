b542e20cf2208e7c0a9c7dc1a98b9c4dee798d78
José Ferraz-Caetano <124404174+jfcaetano@users.noreply.github.com>
2024-02-06 11:53:32 -0500

    Add files via upload

--- Scripts/model_report_analysis.py
+++ Scripts/model_report_analysis.py
@@ -44,3 +44,38 @@ features_df = pd.DataFrame(data, columns=['Feature', 'Average Importance', 'Std 
 
 # Export to new CSV
 features_df.to_csv('summary.csv', index=False)
+
+
+####### GROUP BY DESCRIPTOR FAMILIES
+
+import pandas as pd
+
+# Load the family data
+df_family = pd.read_csv('Group-Lists.csv')
+
+# Create lists for each family, excluding NaN values
+families = {'Ligand' : [item for item in df_family['Ligand'] if pd.notna(item)],
+'Catalyst' : [item for item in df_family['Catalyst'] if pd.notna(item)],
+'Substrate' : [item for item in df_family['Substrate'] if pd.notna(item)],
+'Solvent' : [item for item in df_family['Solvent'] if pd.notna(item)],
+'Reaction' : [item for item in df_family['Reaction'] if pd.notna(item)],
+'Engineered' : [item for item in df_family['Engineered'] if pd.notna(item)]}
+
+
+# Create a mapping of feature to family
+feature_to_family = {}
+for family, features in families.items():
+    for feature in features:
+        feature_to_family[feature] = family
+
+# Load the existing features importance DataFrame
+df_features = pd.read_csv('summary.csv')
+
+# Map each feature to its family
+df_features['Family'] = df_features['Feature'].map(feature_to_family)
+
+# Handle features not found in any family by assigning a default category (optional)
+df_features['Family'].fillna('Other', inplace=True)
+
+# Export the updated DataFrame to CSV
+df_features.to_csv('updated_list.csv', index=False)
